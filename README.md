34e7f247879fca000408d2af56285b116dc7f25a
rob <romijndersrob@gmail.com>
2020-11-12 12:56:00 +0100

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Credits
-
-Template used from https://github.com/saadpasta/developerFolio
-
-## License
-
-This project is licensed under the GPL-3.0 License - see the [LICENSE](./LICENSE) file for details
---
+++ precache-manifest.3ef7e983f855c539b9c87bed635abb90.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6724b5778d19ee905bff371d40f88253",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e57d104d4ffb82ba8f9",
+    "url": "/static/css/3.c7b31c95.chunk.css"
+  },
+  {
+    "revision": "9a41f650065c82e51cfb",
+    "url": "/static/css/4.8297c20c.chunk.css"
+  },
+  {
+    "revision": "f7f5a0fa7f3434121fd1",
+    "url": "/static/css/main.31c072ff.chunk.css"
+  },
+  {
+    "revision": "3126503ca909ace569c3",
+    "url": "/static/js/2.00e29a00.chunk.js"
+  },
+  {
+    "revision": "6e57d104d4ffb82ba8f9",
+    "url": "/static/js/3.4226e5e9.chunk.js"
+  },
+  {
+    "revision": "9a41f650065c82e51cfb",
+    "url": "/static/js/4.9627d0ee.chunk.js"
+  },
+  {
+    "revision": "f7f5a0fa7f3434121fd1",
+    "url": "/static/js/main.f28ef69a.chunk.js"
+  },
+  {
+    "revision": "10db04375a3d9be4d44d",
+    "url": "/static/js/runtime-main.11a16352.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "33f64b0eb7af0aed84d7565bbf2e2acb",
+    "url": "/static/media/frosha_logo.33f64b0e.png"
+  },
+  {
+    "revision": "ec9e27183545878547e9ce9e549792f8",
+    "url": "/static/media/google_logo.ec9e2718.png"
+  },
+  {
+    "revision": "480817be1449089591281eb3d7d37dcb",
+    "url": "/static/media/itsc.480817be.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "2d522f001a4fea98f9b79c6e35f04dc3",
+    "url": "/static/media/rnn_bb.2d522f00.png"
+  },
+  {
+    "revision": "69d681e167f029f58c3e8d343f4943f3",
+    "url": "/static/media/rnn_bb_2.69d681e1.png"
+  },
+  {
+    "revision": "8d090660896fc733b7f3b2083794a239",
+    "url": "/static/media/rob_6__square__lowres.8d090660.jpg"
+  },
+  {
+    "revision": "92aa0b27bb9ce335d41f5e6a0e769fdd",
+    "url": "/static/media/robust.92aa0b27.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "6036f31f1e2194b2e7d3e200cc201303",
+    "url": "/static/media/uada.6036f31f.png"
+  },
+  {
+    "revision": "788271713875e612ecec5fd918ae4446",
+    "url": "/static/media/vivi_objects.78827171.png"
+  }
+]);
